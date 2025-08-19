adfee4067594964c0e5e8723bc81bdd09404bd7d
Marcos-DGalarza <marcos.g3464@gmail.com>
2025-08-19 19:07:40 -0300

    configurando la conexión con la base de datos

--- src/db.js
+++ src/db.js
@@ -1,13 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-  }
-);
+const sequelize = new Sequelize("CarpinChords", "root", "chululu", {
+  host: "localhost",
+  dialect: "mysql"
+});
 
 export default sequelize;
